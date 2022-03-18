4c1f64b5c8c71d800f844c5694e70b013cd14957
vaeilkim <vaeilkim@naver.com>
2022-03-18 21:57:27 +0900

    last commit

--- LabAssignment2/2/2.py
+++ LabAssignment2/2/2.py
@@ -4,33 +4,41 @@ from OpenGL.GL import *
 def render():
     glClear(GL_COLOR_BUFFER_BIT)
     glLoadIdentity()
-    if key == 1:
-        glBegin(GL_POINTS)
-    elif key == 2:
-        glBegin(GL_LINES)
-    elif key == 3:
-        glBegin(GL_LINE_STRIP)
-    elif key == 4:
-        glBegin(GL_LINE_LOOP)
-    elif key == 5:
-        glBegin(GL_TRIANGLES)
-    elif key == 6:
-        glBegin(GL_TRIANGLE_STRIP)
-    elif key == 7:
-        glBegin(GL_TRIANGLE_FAN)
-    elif key == 8:
-        glBegin(GL_QUADS)
-    elif key == 9:
-        glBegin(GL_QUAD_STRIP)
-    elif key == 10:
-        glBegin(GL_POLYGON)
+    glBegin(GL_input)
     glVertex2f(0.5, 0.5)
     glVertex2f(-0.5,0.5)
     glVertex2f(-0.5,-0.5)
     glVertex2f(0.5,-0.5)
     glEnd()
+
+def key_callback(window, key, scancode, action, mods):
+    global GL_input
     
+    if key == glfw.KEY_0:
+        GL_input = GL_POLYGON
+    elif key == glfw.KEY_1:
+        GL_input = GL_POINTS
+    elif key == glfw.KEY_2:
+        GL_input = GL_LINES
+    elif key == glfw.KEY_3:
+        GL_input = GL_LINE_STRIP
+    elif key == glfw.KEY_4:
+        GL_input = GL_LINE_LOOP
+    elif key == glfw.KEY_5:
+        GL_input = GL_TRIANGLES
+    elif key == glfw.KEY_6:
+        GL_input = GL_TRIANGLE_STRIP
+    elif key == glfw.KEY_7:
+        GL_input = GL_TRIANGLE_FAN
+    elif key == glfw.KEY_8:
+        GL_input = GL_QUADS
+    elif key == glfw.KEY_9:
+        GL_input = GL_QUAD_STRIP
+        
 def main():
+    global GL_input
+    GL_input = GL_LINE_LOOP
+
     if not glfw.init():
         return
     
@@ -38,18 +46,16 @@ def main():
     if not window:
         glfw.terminate()
         return
-    glfw.make_context_current(window)
 
-    global key
-    key = 4
+    glfw.set_key_callback(window, key_callback)    
+
+    glfw.make_context_current(window)
+    
     while not glfw.window_should_close(window):
-        try:
-            glfw.poll_events()
-            render()
-            glfw.swap_buffers(window)
-            key = int(input())
-        except EOFError:
-            break
+        glfw.poll_events()
+        render()
+        glfw.swap_buffers(window)
+
     glfw.terminate()
 
 if __name__ == "__main__":
